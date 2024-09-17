96ff01bd2bd69930b581dd592d2a2268c9ecb558
Gautam Das <gdas2005gdas@gmail.com>
2024-09-17 20:38:23 +0930

    try

--- ArrMin/ArrMin.asm
+++ ArrMin/ArrMin.asm
@@ -1,116 +1,65 @@
+// ArrMin.asm
+// This program finds the smallest value in a given array and stores it in R0
+
+// Initialize pointers
 @R1
-D=M
-@R2
-D=M+D
-@16383
-D=A-D
-@END
-D;JLT // end early if array exceeds address 16383
-
-
-@R1
-D=M
-
-@END
-D=D-1
-D=D-1
-D;JLE // end early if array starts at or before address 2
-
-@R2
-D=M
-@END
-D;JLE // end if length < 0
-
-// set min
-@R1
-A=M
-D=M
-@R0
-M=D 
-
-
-
-(Loop)
-@R2
-D = M
-
-@R1
-D=D-1
-A = M + D
-D = M
-
-@BPOS
-D;JGE
-
-@R0
-D = M
-@BNEGAPOS
-D;JGE
-
-@NNPP
-A;JMP
-
-(BPOS)
-@R0
-D = M
-@keepTemp
-D;JLE
-//CONTINUE WITH both neg both positive program
-@NNPP
-A;JMP
-
-
-(BNEGAPOS)
-@R2
-D = M
-
-@R1
-D=D-1
-A = M + D
-D = M
-
-@R0
-M=D
-@keepTemp
-A;JMP
-
-(NNPP)
-@R2
-D = M
-
-@R1
-D=D-1
-A = M + D
-D = M
-
-@R0
-D = M-D
-
-@keepTemp
-D;JLE
-
-@R2
-D=M
-
-@R1
-D=D-1
-A = M + D
-D = M
-
-@R0
+D=M        // D = address of the first element of the array (R1)
+@addr      // Store the base address of the array in addr
 M=D
 
-@R4
-M = D
-
-(keepTemp)
 @R2
-M = M-1
-D = M
+D=M        // D = length of the array (R2)
+@length
+M=D        // Store the length in memory
 
-@Loop
-D;JGT
+// Set the initial minimum value to the first element of the array
+@addr
+A=M        // A = address of the first element
+D=M        // D = *addr (value of the first element)
+@R0
+M=D        // Set R0 = initial minimum
+
+// Start the loop
+@i
+M=1        // i = 1 (since we already took the first element)
+
+(LOOP)
+    @i
+    D=M    // D = i (current index)
+    @length
+    D=D-M  // D = i - length
+    @END
+    D;JGE  // If i >= length, go to END
+
+    // Compare current element with the minimum
+    @addr
+    D=M    // D = base address of array
+    @i
+    D=D+M  // D = addr + i (current element address)
+    A=D    // A = RAM[addr + i]
+    D=M    // D = RAM[addr + i] (current element)
+    @R0
+    D=D-M  // D = current element - R0 (current min)
+    @SKIP
+    D;JGE  // If current element >= min, skip to SKIP
+
+    // Update min if the current element is smaller
+    @addr
+    D=M    // D = base address
+    @i
+    D=D+M  // D = addr + i
+    A=D    // A = RAM[addr + i]
+    D=M    // D = current element
+    @R0
+    M=D    // R0 = current element (new min)
+
+(SKIP)
+    // Increment i and repeat
+    @i
+    M=M+1
+    @LOOP
+    0;JMP  // Go back to LOOP
 
 (END)
-@END
-A;JMP
+    @END
+    0;JMP  // Infinite loop at the end
