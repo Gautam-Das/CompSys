7b769ba08d58aacc0c8cdc61d3f5d464cc983213
Gautam Das <gdas2005gdas@gmail.com>
2024-09-18 00:32:03 +0930

    changed sorting

--- ArrSort/ArrSort.asm
+++ ArrSort/ArrSort.asm
@@ -1,115 +1,122 @@
-@R1
-D=M
-@R3
-M=D
-
+// i, j, temp;
 @R2
-D=M
-@R4
+D=M-1
+@I
 M=D
-
-@R5
+@TEMP
 M=0
 
-(GETMIN)
-    @R1
-    A=M //get address if current element
-    D=M //store value of current element
-    @R0
-    M=D //store  first value in minimum
+//    for (i = (array_size - 1); i>= 0; i--)
+(ILOOP)
+    @I
+    D=M
+    @END
+    D;JLT
+    @J
+    M=1
 
-    (WHILE)
-        // check if R2 = 0
-        @R2
+    (JLOOP)
+        @J
         D=M
-        @CHANGEARRAY
-        D;JEQ
-        
-        // Decrement  R2
-        @R2
-        M=M-1
+        @I
+        D=M-D // I-J
+        @ILOOPCONT
+        D;JLT
 
-        // Get curretn element
-        @R1 
-        A=M
+        @J
         D=M
-
-        // find difference between current minimum and new minimum
-        @R0
-        D=M-D
-        
-        // jump if M-D>0 i.e. M>D
-        @minimum
+        @R1
+        A=D+M
+        D=M
+        @JPOS
         D;JGT
+        @JNEG
+        D;JMP
         
-        @continue
-        0;JMP
 
-
-        (minimum)
-            // Track address of minimum element
+        //if (numbers[j-1] > numbers[j])
+        (SWAP)
+            @J
+            D=M-1
             @R1
-            D=M
-            @R5
+            A=D+M
+            D=M // D= J-1
+            @TEMP
             M=D
 
-            // Get current element
-            @R1 
+            @J
+            D=M
+            @R1
+            A=D+M
+            D=M // D= J
+            A=A-1 // A POINTS TO J-1
+            M=D
+
+            A=A+1 // A POINTS TO J
+            D=A // D POINTS TO J
+            
+            @JADDRESS
+            M=D
+
+            @TEMP
+            D=M
+
+            @JADDRESS
             A=M
-            D=M
-            //  change  minimum
-            @R0
             M=D
-            @continue 
-            0;JMP
 
-        (continue)
-            // increase memory address
-            @R1
+
+        (CONTINUE)
+            @J
             M=M+1
-            @WHILE  
+            @JLOOP
             0;JMP
-
     
-(CHANGEARRAY)
-@R3
-A=M
-D=M
-@R5
-A=M
-M=D
-
-@R0
-D=M
-@R3
-A=M
-M=D
-@R3
-M=M+1
-
-@R4
-DM=M-1
-@END
-D;JEQ
-
-@R3
-D=M
-@R1
-M=D
-
-@R4
-D=M
-@R2
-M=D
-
-@R0
-M=-1
-
-@GETMIN
-0;JMP
-
-
-
+    (ILOOPCONT)
+        @I
+        M=M-1
+        @ILOOP
+        0;JMP
+    
 (END)
+    @R0
+    M=-1
     @END
     0;JMP
+
+(JPOS)
+    // if j-1 < 0, then j-1<j else check diff
+    @J
+    D=M-1
+    @R1
+    A=D+M
+    D=M
+    @CONTINUE
+    D;JLT
+    @DIFF
+    D;JMP
+
+(JNEG)
+    // if j-1 >0 , then j-1>j else check diff
+    @J
+    D=M-1
+    @R1
+    A=D+M
+    D=M
+    @SWAP
+    D;JGT
+    @DIFF
+    D;JMP
+
+(DIFF)
+    @J
+    D=M-1
+    @R1
+    A=D+M
+    D=M // D= J-1
+    A=A+1 // A POINTS TO J
+    D=D-A // (J-1)-J
+    @SWAP
+    D;JGT
+    @CONTINUE
+    D;JMP
