4e9fb2cbdd9912163ef27e300319b83849927ead
Gautam Das <gdas2005gdas@gmail.com>
2024-09-17 17:07:22 +0930

    different

--- ArrMin/ArrMin.asm
+++ ArrMin/ArrMin.asm
@@ -1,61 +1,124 @@
-@R1
-A=M //get address if current element
-D=M //store value of current element
+// Finds the smallest element in the array of length R2 whose first element is at RAM[R1] and stores the result in R0.
+// (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
+// Put your code here.
+//Check if array start far above R2
+@R1
+D=M
+@R2
+D=M+D
+@16383
+D=A-D
+@END
+D;JLT
+
+
+@R1
+D=M
+
+@END
+D=D-1
+D=D-1
+D;JLE
+
+//get current tempMin
+@R1
+A=M
+D=M
+
+@R0
+M=D
+
+//Check that length of array is sufficient
 @R2
 D=M
+
 @END
 D;JLE
 
+
+
+(Loop)
+@R2
+D = M
+
+@R1
+D=D-1
+A = M + D
+D = M
+
+@BPOS
+D;JGE
+
 @R0
-M=D //store  first value in minimum
+D = M
+@BNEGAPOS
+D;JGE
 
-(WHILE)
-    // check if R2 = 0
-    @R2
-    D=M
-    @END
-    D;JEQ
-    
-    // Decrement  R2
-    @R2
-    M=M-1
+@NNPP
+A;JMP
 
-    // Get curretn element
-    @R1 
-    A=M
-    D=M
-
-    // find difference between current minimum and new minimum
-    @R0
-    D=M-D
-    
-    // jump if M-D>0 i.e. M>D
-    @minimum
-    D;JGT
-    
-    @continue
-    0;JMP
+(BPOS)
+@R0
+D = M
+@keepTemp
+D;JLE
+//CONTINUE WITH both neg both positive program
+@NNPP
+A;JMP
 
 
-    (minimum)
-        // Get current element
-        @R1 
-        A=M
-        D=M
-        //  change  minimum
-        @R0
-        M=D
-        @continue 
-        0;JMP
+(BNEGAPOS)
+@R2
+D = M
 
-    (continue)
-        // increase memory address
-        @R1
-        M=M+1
-        @WHILE  
-        0;JMP
+@R1
+D=D-1
+A = M + D
+D = M
+
+@R0
+M=D
+@keepTemp
+A;JMP
+
+(NNPP)
+@R2
+D = M
+
+@R1
+D=D-1
+A = M + D
+D = M
+
+@R0
+D = M-D
+
+@keepTemp
+D;JLE
+
+@R2
+D=M
+
+@R1
+D=D-1
+A = M + D
+D = M
+
+@R0
+M=D
+
+@R4
+M = D
+
+(keepTemp)
+@R2
+M = M-1
+D = M
+
+@Loop
+D;JGT
 
 (END)
-    @END
-    0;JMP
+@END
+A;JMP
