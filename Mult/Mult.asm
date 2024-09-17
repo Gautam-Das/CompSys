872960f3cecc25f252f4265e23b45b303fabb5d8
Gautam Das <gdas2005gdas@gmail.com>
2024-09-17 15:28:10 +0930

    mult changes

--- Mult/Mult.asm
+++ Mult/Mult.asm
@@ -1,19 +1,56 @@
 @R0
 M=0
 
-(WHILE)
-    @R2
-    D=M
-    @END
-    D;JLE
-    @R2
-    M=M-1
-    @R1
-    D=M
-    @R0
-    M=D+M
-    @WHILE
-    0;JMP
+@R1
+D=M
+@END
+D;JEQ
+
+@R2
+D=M
+@END
+D;JEQ
+@NEG
+D;JLT
+@POS
+D;JGT
+
+(NEG)
+    (WHILE1)
+        @R2
+        D=M
+        @END1
+        D;JGE
+        @R2
+        M=M+1
+        @R1
+        D=M
+        @R0
+        M=D+M
+        @WHILE1
+        0;JMP
+
+    (END1)
+        @R0
+        M=-M
+        @END
+        0;JMP
+
+(POS)
+    (WHILE2)
+        @R2
+        D=M
+        @END
+        D;JLE
+        @R2
+        M=M-1
+        @R1
+        D=M
+        @R0
+        M=D+M
+        @WHILE2
+        0;JMP
+    
 
 (END)
     @END
